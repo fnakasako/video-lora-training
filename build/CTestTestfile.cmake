bdf79afce64ffae0cc41ec7ad7638065d8f30d87
fnakasako <frankbnakasako@gmail.com>
2025-07-13 23:57:14 -0400

    training pipeline

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/frank/Desktop/company/video-lora-training/video-lora-training
+# Build directory: /Users/frank/Desktop/company/video-lora-training/video-lora-training/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(VideoPipelineTests "/Users/frank/Desktop/company/video-lora-training/video-lora-training/build/run_tests")
+set_tests_properties(VideoPipelineTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/frank/Desktop/company/video-lora-training/video-lora-training/CMakeLists.txt;62;add_test;/Users/frank/Desktop/company/video-lora-training/video-lora-training/CMakeLists.txt;0;")
